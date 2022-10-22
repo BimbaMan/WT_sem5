51592902ce11c7cc7ea167f854d497910c96647c
BimbaMan <ivan.d.sidorenko@gmail.com>
2022-10-22 15:51:32 +0300

    .xml changes

--- LR2/src/main/java/DAO/impl/ApplianceDAOImpl.java
+++ LR2/src/main/java/DAO/impl/ApplianceDAOImpl.java
@@ -1,4 +1,5 @@
 package DAO.impl;
 
 public class ApplianceDAOImpl {
+
 }
--- LR2/src/main/java/main/Main.java
+++ LR2/src/main/java/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import DAO.IApplianceDAO;
+import DAO.impl.ApplianceDAOImpl;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Main(IApplianceDAO applianceDAO) {
+        this.applianceDAO = applianceDAO;
+    }
+
+    private final IApplianceDAO applianceDAO;
+
+    public static void main(String[] args){
+//        Main main = new Main(new ApplianceDAOImpl(DBLoader.loadAllFromDB()));
+//        main.applianceDAO.findAllAppliancesByType("Oven")
+//                .forEach(appliance -> System.out.println("appliance = " + appliance));
+//        System.out.println(main.applianceDAO.findCheapest());
     }
 }
